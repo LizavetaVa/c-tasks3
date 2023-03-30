b8375303f9f79752403ee8a944b42a7ccd835162
Lizaveta Valynets <volynets2000@mail.ru>
2023-03-30 22:47:24 +0300

    done

---
+++ task19/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите пятизначиное число: ");
+bool x = true;
+
+while (x)
+{
+    int N = int.Parse(Console.ReadLine()!); 
+
+    if (N > 9999 && N < 100000)
+    {
+        int a = N / 10000;
+        Console.WriteLine("Первая цифра: " + a);
+
+        int b = N / 1000 - a * 10;
+        Console.WriteLine("Вторая цифра: " + b);
+
+        int c = N / 100 - b * 10  - a * 100;
+        Console.WriteLine("Третья цифра: " + c);
+
+        int d = N / 10 - c * 10 - b * 100  - a * 1000;
+        Console.WriteLine("Четвертая цифра: " + d);
+
+        int e = N - d * 10 - c * 100 - b * 1000  - a * 10000;
+        Console.WriteLine("Пятая цифра: " + e);
+
+
+    if(a == e && b == d)
+        Console.WriteLine($"Число {N} является палиндромом.");
+    else
+        Console.WriteLine($"Число {N} не является палиндромом.");
+        x = false;
+}
+
+    else 
+        Console.WriteLine($"Число {N} не является ппятизначным введите пятизначное.");
+}
