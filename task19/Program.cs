fe59c195fe95b28277111917c0729f445ef311f5
Lizaveta Valynets <volynets2000@mail.ru>
2023-03-30 22:51:11 +0300

    done

--- task19/Program.cs
+++ task19/Program.cs
@@ -8,19 +8,19 @@ while (x)
     if (N > 9999 && N < 100000)
     {
         int a = N / 10000;
-        Console.WriteLine("Первая цифра: " + a);
+        Console.WriteLine("Первая цифра : " + a);
 
         int b = N / 1000 - a * 10;
-        Console.WriteLine("Вторая цифра: " + b);
+        Console.WriteLine("Вторая цифра : " + b);
 
         int c = N / 100 - b * 10  - a * 100;
-        Console.WriteLine("Третья цифра: " + c);
+        Console.WriteLine("Третья цифра : " + c);
 
         int d = N / 10 - c * 10 - b * 100  - a * 1000;
-        Console.WriteLine("Четвертая цифра: " + d);
+        Console.WriteLine("Четвертая цифра : " + d);
 
         int e = N - d * 10 - c * 100 - b * 1000  - a * 10000;
-        Console.WriteLine("Пятая цифра: " + e);
+        Console.WriteLine("Пятая цифра : " + e);
 
 
     if(a == e && b == d)
