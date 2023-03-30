35a659e0ab460378a893e004f9cd82e1d4c6f4e4
Lizaveta Valynets <volynets2000@mail.ru>
2023-03-30 22:49:57 +0300

    done

---
+++ task23/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число N: ");
+bool x = true;
+int i = 1;
+
+while (x)
+{
+    int N = int.Parse(Console.ReadLine()!); 
+
+    if(N > 1)
+    {
+        for (i = 1; i < N; i++)
+    {
+        Console.Write(Math.Pow(i, 3) + " , ");
+        x = false;
+    }
+    }
+
+    else
+    {
+        Console.WriteLine("N должно быть больше 1, введите его еще раз: ");
+    }
+}
+Console.Write(Math.Pow(i, 3));
